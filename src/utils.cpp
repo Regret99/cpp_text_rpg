f5e16857603f876863b18d4911b818a35b2e437a
Rajat <rajat@jobma.com>
2025-08-28 13:11:26 +0530

    added utilty methods

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.h"
 #include <iostream>
+#include <boost/lexical_cast.hpp>
+#include <string>
 
 
 using namespace std;
@@ -38,4 +40,44 @@ void minimenu() {
 	cout << "This is a menu.\n";
 
 
+}
+
+int int_to_string(string string_in)
+{
+    int i = 0;
+    try {
+        i = boost::lexical_cast<int>(string_in);
+    } catch (...) {
+        return -1; // Returns -1 if exception raised
+    }
+    return i;
+}
+
+int int_option(int max_digit)
+{
+    string prompt = "Enter a value between 1 and " + to_string(max_digit) + ": ";
+    // print(prompt, "blue", false);
+	cout<<prompt<<endl;
+
+    if (cin.peek() == '\n')
+        cin.ignore();
+
+    bool valid_in = false;
+    string stringIn = "-1";
+    int answer;
+    while (!valid_in) {
+        getline(cin, stringIn);
+        if (stringIn.length() == 1) {
+            answer = int_to_string(stringIn);
+        }
+        if (answer > 0 && answer <= max_digit) {
+            valid_in = true;
+        } else {
+            // print("Invalid option. Try again...", "red");
+            // print(prompt, "blue", false);
+			cout << "Invalid option. Try again..." << endl;
+			cout << prompt << endl;
+        }
+    }
+    return answer;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,11 @@
 #pragma once
 #include<iostream>
+#include <string>
+
+using namespace std;
 
 void minimenu();
 void mainMenu();
 void inGameMenu();
+int int_option(int max_digit);
+int int_to_string(string string_in);
